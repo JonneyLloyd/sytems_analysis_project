78fbb6c6f7d2ed990013cf155024b272a9d55cfd
JonneyLloyd <jonneylloyd@yahoo.co.uk>
2017-04-17 09:22:19 +0000

    Jonney frontend for use cases

--- code/app/controllers/frontend/rooms.py
+++ code/app/controllers/frontend/rooms.py
@@ -3,6 +3,7 @@ from flask import current_app as app
 
 from app.api.room_manager import RoomManager
 from app.forms.rooms import RoomAvailabilityForm
+from app.forms.rooms import SetRoomCleanForm
 
 @app.route('/rooms/rooms', methods=['GET', 'POST'])
 def rooms():
@@ -12,3 +13,15 @@ def rooms():
         return render_template("rooms/roomview.html",result = result)
 
     return render_template('rooms/rooms.html', form=form)
+
+@app.route('/rooms/room-clean', methods=['GET', 'POST'])
+def room_clean():
+    form = SetRoomCleanForm()
+    if form.validate_on_submit():
+        clean_result = RoomManager.set_room_clean(number=form.number.data, clean=form.clean.data)
+        available_result = RoomManager.set_room_availablity(number=form.number.data, availability=form.available.data)
+        if clean_result and available_result:
+            result = RoomManager.get_room(number=form.number.data)
+            return render_template("rooms/roomview.html",result = result)
+
+    return render_template('rooms/room-clean.html', form=form)
