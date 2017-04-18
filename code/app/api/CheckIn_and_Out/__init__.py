fb3bf3af6ae727fbc8f9be36084a11db46a8fff8
OliverGavin <14161044@studentmail.ul.ie>
2017-04-18 23:33:29 +0100

    Refactored and simplified code. Removed circular dependencies. Renamed objects. Removed redundant code.

--- code/app/api/CheckIn_and_Out/__init__.py
+++
@@ -1,1 +0,0 @@
-from .CheckInManager import CheckInManager
--- code/app/controllers/frontend/checkin_and_out.py
+++ code/app/controllers/frontend/checkin_and_out.py
@@ -1,64 +1,64 @@
-from flask import render_template, redirect, url_for, request, session,flash
+from flask import render_template, redirect, url_for, request, session, flash
 from flask import current_app as app
 
 from app.api.user_manager import UserManager
-from app.forms.checkin import CheckInForm,CheckOutForm
-from app.api.CheckIn_and_Out import CheckInManager
+from app.forms.checkin import CheckInForm, CheckOutForm
+from app.api.checkin_manager import CheckInManager
 
-@app.route('/checkin_and_out/surecheckin',methods=['GET','POST'])
+
+@app.route('/checkin_and_out/checkin', methods=['GET', 'POST'])
 def checkin():
-    user_id = request.args.get('user_id')
-    user = UserManager.get_user_by_id(user_id)
-    bookings = CheckInManager.getBookingOB(user.first_name, user.last_name, user.credit_num)
-    # TODO: check permissions
-    if request.method == 'POST':
-        var=int(request.form['r_num']);
-        CheckInManager.change_avaliability(var)
-        print('####', var, CheckInManager.get_Firstname())
-        CheckInManager.update_Database(CheckInManager.get_Firstname(),CheckInManager.get_Lastname(),CheckInManager.get_CreditNum())
-        return render_template('checkin_and_out/AcceptCheckIn.html', bookings=bookings, user=user)
+    if 'booking_user_id' not in session or 'booking_credit_num' not in session:
+        return redirect(url_for('checkin_form'))
 
-@app.route('/checkin_and_out/checkinform',methods=['GET','POST'])
-def verify():
-    form=CheckInForm()
+    user = UserManager.get_user_by_id(session['booking_user_id'])
+    credit_num = session['booking_credit_num']
+
+    # An exit button should clear the session cookies!!!
+    # del session['booking_user_id']
+    # del session['booking_credit_num']
+
+    bookings = CheckInManager.getBookings(user.details.first_name, user.details.last_name, credit_num)
+
+    if request.method == 'POST':
+        var = int(request.form['r_num'])
+        CheckInManager.check_in(var)
+
+    return render_template('checkin_and_out/AcceptCheckIn.html', bookings=bookings, user=user)
+
+
+@app.route('/checkin_and_out/checkinform', methods=['GET', 'POST'])
+def checkin_form():
+    form = CheckInForm()
     if form.validate_on_submit():
         first_name = form.first_name.data
         last_name = form.last_name.data
         credit_num = form.credit_num.data
-        # form=CheckInManager.getForm();
-        # form.Judge(fir, las, cre);
 
-        bookings = CheckInManager.getBookingOB(first_name, last_name, credit_num)  # an attribute to record the judge result, if the user is valid.
-        user = bookings[0].user
+        # This implementation (for unauthenticated users works)
+        # An alternatice could be added for logged in users too
+        bookings = CheckInManager.getBookings(first_name, last_name, credit_num)
 
-        if (len(bookings) > 0):
-            leng = len(bookings);
-            # CheckInManager.set_CreditNum(credit_num)
-            # CheckInManager.set_Firstname(first_name)
-            # CheckInManager.set_Lastname(last_name)
-            # return render_template('checkin_and_out/AcceptCheckIn.html', bookings=bookings, user=user)
-            print('#######redirect')
-            redirect(url_for('checkin'))
-            print('#######after')
-            # redirect(url_for('checkin', user_id=user.id))
+        if (bookings):
+            user = bookings[0].user
+            session['booking_user_id'] = user.id
+            session['booking_credit_num'] = credit_num
+            return redirect(url_for('checkin'))
         else:
-            flash('Sorry,the Name or CreditCard number is wrong or not bookings could be found\n', 'danger')
-    return render_template('checkin_and_out/CheckIn.html',fm=form)
+            flash('Sorry, the Name or Credit Card number is wrong or no bookings could be found\n', 'danger')
 
-# @app.route('/checkin_and_out/checkinform',methods=['GET','POST'])
-# def createForm():
-#     form=CheckInForm();
-#     # CheckInManager.setForm(form);
-#     return render_template('checkin_and_out/CheckIn.html',fm=form)
+    return render_template('checkin_and_out/CheckIn.html', fm=form)
 
-@app.route('/checkin_and_out/checkoutform',methods=['GET','POST'])
+
+@app.route('/checkin_and_out/checkoutform', methods=['GET', 'POST'])
 def checkout():
-    form=CheckOutForm();
-    if request.method == 'POST':
-        credit=request.form['credit_num'];
-        if(form.Judge(credit)):
-            print('Yes');
-            return render_template('/checkin_and_out/AcceptCheckout.html')
+    form = CheckOutForm()
+    if form.validate_on_submit():
+        credit_num = form.credit_num.data
+
+        if CheckInManager.check_out(credit_num):
+            return render_template('/checkin_and_out/AcceptCheckOut.html')
         else:
-            flash('Your credit card number is incorrect or you have no records to checkout','danger')
-    return render_template('checkin_and_out/CheckOut.html',fm=form)
+            flash('Your credit card number is incorrect or you have no records to checkout', 'danger')
+
+    return render_template('checkin_and_out/CheckOut.html', fm=form)
