4f55bb85f7e25b5a4e535ec395b6858b1da07b46
JonneyLloyd <jonneylloyd@yahoo.co.uk>
2017-04-18 22:56:25 +0000

    Jonney unit tests

--- code/app/controllers/frontend/booking.py
+++ code/app/controllers/frontend/booking.py
@@ -5,6 +5,7 @@ from app.api.booking_view import BookingView
 from app.api.room_manager import RoomManager
 from app.forms.booking import BookingForm
 from flask import render_template, redirect, url_for, request, session
+from app.auth.login import LoginManager, login_required
 
 from app.api.booking_manager import cancelBooking, makeBooking
 from app.api.change_room_price import changePrice
@@ -12,15 +13,18 @@ from app.api.booking_manager import cancelBooking, makeBooking
 from app.api.change_room_price import changePrice
 
 @app.route('/booking/booking', methods=['GET', 'POST'])
+@login_required
 def booking():
     form = BookingForm()
     if form.validate_on_submit():
-        result = BookingView.get_booking_for_user(user_id=form.user_id.data)
-        return render_template("booking/bookingView.html",result = result)
+        result = BookingView.get_booking_for_user(g.user.id)
+        if result:
+            return render_template("booking/bookingView.html",result = result)
 
     return render_template('booking/bookingForm.html', form=form)
 
 @app.route('/booking/bookingForm')
+@login_required
 def booking_view():
     return render_template('booking/bookingForm.html')
 
@@ -89,68 +93,3 @@ def change_price():
     user_id = g.user.id
     changePrice.pricechange(user_id, room_id, weekday_price, weekend_price)
     return render_template('booking/priceChanged.html')
-
-'''
-Quick test for sales and room manager. Will remove
-'''
-
-
-
-print "\nTEST get_rooms_occupied_on_date"
-
-print RoomManager.get_rooms_occupied_on_date("2017-01-01", 1)
-print "END TEST\n"
-
-RoomManager.set_availability_for_booking("2017-01-01", 1)
-RoomManager.set_availability_for_booking("2017-01-01", 1)
-RoomManager.set_availability_for_booking("2017-01-01", 1)
-RoomManager.set_availability_for_booking("2017-01-01", 1)
-
-print "\nTEST get_rooms after decrease"
-print RoomManager.get_rooms_occupied_on_date("2017-01-01", 1)
-print "END TEST\n"
-
-print "\nTEST increase booking"
-RoomManager.increase_availability_for_booking("2017-01-01", 1)
-RoomManager.increase_availability_for_booking("2017-01-01", 1)
-RoomManager.increase_availability_for_booking("2017-01-01", 1)
-RoomManager.increase_availability_for_booking("2017-01-01", 1)
-
-print RoomManager.get_rooms_occupied_on_date("2017-01-01", 1)
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.get_room_price_from_type('single')
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.get_room_price_from_number(101)
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.get_room(101)
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.get_room_availablity(101)
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.get_room_clean(101)
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.set_room_availablity(101, 'on fire')
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.set_room_clean(101,  False)
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.get_room_availablity(101)
-print "END TEST\n"
-
-print "\nTEST"
-print RoomManager.get_room_clean(101)
-print "END TEST\n"
--- code/app/forms/booking.py
+++ code/app/forms/booking.py
@@ -1,11 +1,10 @@
 from flask import flash
 from flask_wtf import FlaskForm as Form
-from wtforms import StringField, PasswordField, ValidationError
+from wtforms import StringField, IntegerField, ValidationError
 from wtforms.validators import Required, Email, EqualTo
 from app.api.booking_view import BookingView
 
 class BookingForm(Form):
-    user_id = StringField('User ID', [Required()])
 
     def __init__(self, *args, **kwargs):
         super(BookingForm, self).__init__(*args, **kwargs)
@@ -13,10 +12,6 @@ class BookingForm(Form):
     def validate(self):
         valid = super(BookingForm, self).validate()
         if not valid:
-            return False
-        user_id = self.user_id.data
-        if not BookingView.get_booking_for_user(user_id):
-            flash('User not found', 'danger')  # TODO: enum
             return False
         else:
             return True
