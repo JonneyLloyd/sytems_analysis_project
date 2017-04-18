e6568e6f584707fcbaf0ab01e5ef5c64087b31b0
EasternBob <16070461@studentmail.ul.ie>
2017-04-18 14:02:10 +0100

    'tmp2'

--- code/app/controllers/frontend/checkin_and_out.py
+++ code/app/controllers/frontend/checkin_and_out.py
@@ -1,40 +1,55 @@
 from flask import render_template, redirect, url_for, request, session,flash
 from flask import current_app as app
+
+from app.api.user_manager import UserManager
 from app.forms.checkin import CheckInForm,CheckOutForm
 from app.api.CheckIn_and_Out import CheckInManager
+
 @app.route('/checkin_and_out/surecheckin',methods=['GET','POST'])
 def checkin():
+    user_id = request.args.get('user_id')
+    user = UserManager.get_user_by_id(user_id)
+    bookings = CheckInManager.getBookingOB(user.first_name, user.last_name, user.credit_num)
+    # TODO: check permissions
     if request.method == 'POST':
         var=int(request.form['r_num']);
         CheckInManager.change_avaliability(var)
         print('####', var, CheckInManager.get_Firstname())
         CheckInManager.update_Database(CheckInManager.get_Firstname(),CheckInManager.get_Lastname(),CheckInManager.get_CreditNum())
-        form=CheckInManager.getForm();
-        return render_template('checkin_and_out/AcceptCheckIn.html',fm=form)
-
-@app.route('/checkin_and_out/verifyform',methods=['GET','POST'])
-def verify():
-    if request.method == 'POST':
-        fir = request.form['first_name'];
-        las = request.form['last_name'];
-        cre = request.form['credit_num'];
-        form=CheckInManager.getForm();
-        form.Judge(fir, las, cre);
-        if (form.judge is True):
-            leng = len(form.getBookingOb());
-            CheckInManager.set_CreditNum(cre)
-            CheckInManager.set_Firstname(fir)
-            CheckInManager.set_Lastname(las)
-            return render_template('checkin_and_out/AcceptCheckIn.html', fm=form);
-        else:
-            flash('Sorry,the Name or CreditCard number is wrong\n', 'danger');
-    return render_template('checkin_and_out/CheckIn.html',fm=form)
+        return render_template('checkin_and_out/AcceptCheckIn.html', bookings=bookings, user=user)
 
 @app.route('/checkin_and_out/checkinform',methods=['GET','POST'])
-def createForm():
-    form=CheckInForm();
-    CheckInManager.setForm(form);
-    return render_template('checkin_and_out/CheckIn.html',fm=form);
+def verify():
+    form=CheckInForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        credit_num = form.credit_num.data
+        # form=CheckInManager.getForm();
+        # form.Judge(fir, las, cre);
+
+        bookings = CheckInManager.getBookingOB(first_name, last_name, credit_num)  # an attribute to record the judge result, if the user is valid.
+        user = bookings[0].user
+
+        if (len(bookings) > 0):
+            leng = len(bookings);
+            # CheckInManager.set_CreditNum(credit_num)
+            # CheckInManager.set_Firstname(first_name)
+            # CheckInManager.set_Lastname(last_name)
+            # return render_template('checkin_and_out/AcceptCheckIn.html', bookings=bookings, user=user)
+            print('#######redirect')
+            redirect(url_for('checkin'))
+            print('#######after')
+            # redirect(url_for('checkin', user_id=user.id))
+        else:
+            flash('Sorry,the Name or CreditCard number is wrong or not bookings could be found\n', 'danger')
+    return render_template('checkin_and_out/CheckIn.html',fm=form)
+
+# @app.route('/checkin_and_out/checkinform',methods=['GET','POST'])
+# def createForm():
+#     form=CheckInForm();
+#     # CheckInManager.setForm(form);
+#     return render_template('checkin_and_out/CheckIn.html',fm=form)
 
 @app.route('/checkin_and_out/checkoutform',methods=['GET','POST'])
 def checkout():
--- code/app/forms/checkin.py
+++ code/app/forms/checkin.py
@@ -1,30 +1,30 @@
 from flask_wtf import FlaskForm as Form
 from wtforms import StringField,SubmitField,IntegerField
-from wtforms.validators import InputRequired
+from wtforms.validators import Required
 from app.api.CheckIn_and_Out.CheckInManager import CheckInManager
 from app.api.CheckIn_and_Out.CheckOutManager import CheckOutManager
 
 
 class CheckInForm(Form):
-    def _init_(self):
-        self._lis=[];
-    def setlis(self,lis):
-        self._lis=lis;
-    first_name=StringField('Firstname',validators=[InputRequired()])
-    last_name=StringField('Lastname',validators=[InputRequired()])
-    credit_num=IntegerField('Credit',validators=[InputRequired()])
-    submit=SubmitField('Submit')
-    def Judge(self,first_name,last_name,credit_num):
-        result=CheckInManager.getBookingOB(first_name,last_name,credit_num);
-        self.judge=result[1];#an attribute to record the judge result, if the user is valid.
-        self._lis=result[0];
-    def getBookingOb(self):
-        return self._lis
-    def chu(self):
-        print("I was clicked!");
+    # def _init_(self):
+    #     self._lis=[];
+    # def setlis(self,lis):
+    #     self._lis=lis;
+    first_name=StringField('Firstname', [Required()])
+    last_name=StringField('Lastname', [Required()])
+    credit_num=IntegerField('Credit', [Required()])
+    # submit=SubmitField('Submit')
+    # def Judge(self,first_name,last_name,credit_num):
+    #     result=CheckInManager.getBookingOB(first_name,last_name,credit_num);
+    #     self.judge=result[1];#an attribute to record the judge result, if the user is valid.
+    #     self._lis=result[0];
+    # def getBookingOb(self):
+    #     return self._lis
+    # def chu(self):
+    #     print("I was clicked!");
 
 
 class CheckOutForm(Form):
-    credit_num = StringField('CreditNum', validators=[InputRequired()])
+    credit_num = StringField('CreditNum', [Required()])
     def Judge(self,credit_num):
         return CheckOutManager.check_out(credit_num);
