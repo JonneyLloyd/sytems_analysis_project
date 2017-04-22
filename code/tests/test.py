95fcdd6b799c5a6a82e28cb20cf50bcaa8eaf6f7
JonneyLLoyd <jonneylloyd@yahoo.co.uk>
2017-04-22 14:02:38 +0100

    Database setup and teardown now moved to test base class

--- code/tests/test.py
+++ code/tests/test.py
@@ -7,6 +7,17 @@ from app.app_factory import AppFactory
 from app.extensions import db
 from config import TestConfig
 
+from app.api.booking_view import BookingView
+from app.api.room_manager import RoomManager
+from app.api.user_manager import UserManager
+from app.models.user import User
+from app.models.user import UserDetails
+from app.models.room import RoomPrice
+from app.models.room import Room
+from app.models.booking import Booking
+
+from datetime import datetime
+
 
 class BaseDatabaseTest(object):
     '''
@@ -21,7 +32,19 @@ class BaseDatabaseTest(object):
 
         with cls.app.app_context():
             db.create_all()
-            # populate here or in sub class
+            roomPrice = RoomPrice("single", 100, 150)
+            room = Room(1, 101, 3, "Available", 1)
+            booking = Booking(1, 1, datetime.strptime('2017-01-02', '%Y-%m-%d').date()
+            ,datetime.strptime('2017-01-10', '%Y-%m-%d').date(), 123123123, 2000)
+            RoomManager.set_availability_for_booking(datetime.strptime("2017-01-01", '%Y-%m-%d').date(), 1)
+            UserManager.create_user("asd@asd.asd", "asdasd")
+            user = UserManager.get_user("asd@asd.asd")
+            UserManager.update_details(user, "mr", "test", "05644654")
+
+            db.session.add(roomPrice)
+            db.session.add(room)
+            db.session.add(booking)
+            db.session.commit()
 
     @classmethod
     def teardown_class(cls):
